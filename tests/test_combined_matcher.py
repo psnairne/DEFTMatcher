41e0dea79779264d1e1f293ad97f803d452b94e1
psnairne <patricknairne@hotmail.co.uk>
2026-03-10 19:17:47 +0100

    new tests

---
+++ tests/test_combined_matcher.py
@@ -0,0 +1,14 @@
+from deft_matcher.matchers.combined_matcher import CombinedMatcher
+from deft_matcher.ontology_class import OntologyClass
+from scripts.fixtures import hpo_syn_retriever, choose_first_resolver
+
+
+def test_combined_matcher():
+    retriever = hpo_syn_retriever()
+    resolver = choose_first_resolver()
+
+    combined_matcher = CombinedMatcher(retriever, resolver)
+
+    asd_match = combined_matcher.match("ASD")
+
+    assert asd_match == OntologyClass("HP:0000729", "Autistic behavior")
